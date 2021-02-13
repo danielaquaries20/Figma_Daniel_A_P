b7d588e21b3b519fbbc53f8fdd30bcce4640a000
danielaquaries20 <danielaquaries@gmail.com>
2021-02-13 09:54:07 +0700

    Perubahan 1

--- app/src/main/java/com/example/figmadaniel/LoginActivity.kt
+++ app/src/main/java/com/example/figmadaniel/LoginActivity.kt
@@ -2,23 +2,36 @@ package com.example.figmadaniel
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class LoginActivity : AppCompatActivity() {
+    var isShowPassword = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         textLupaKataSandiLogin.onClick {
-            val intent = Intent(this@LoginActivity, LupaSandiActivity::class.java )
+            val intent = Intent(this@LoginActivity, LupaSandiActivity::class.java)
             startActivity(intent)
         }
 
         textRegisterLogin.onClick {
             val intent = Intent(this@LoginActivity, RegstrasiActivity::class.java)
             startActivity(intent)
+        }
+
+        imageVisibleLogin.onClick {
+            if (isShowPassword) {
+                editTextPasswordLogin.transformationMethod =
+                    HideReturnsTransformationMethod.getInstance()
+            } else {
+                editTextPasswordLogin.transformationMethod =
+                    PasswordTransformationMethod.getInstance()
+            }
         }
 
 
