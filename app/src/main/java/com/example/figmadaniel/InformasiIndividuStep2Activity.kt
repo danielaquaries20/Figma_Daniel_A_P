e73dca24df832dc4248f1f62b13e1d30cf5b3e23
danielaquaries20 <danielaquaries@gmail.com>
2021-02-20 11:46:23 +0700

    Perubahan 7, Membuat Layout Tambah Informasi Individu Step 2 dan Membuat Bottomsheet Tambah Data Populasi.

---
+++ app/src/main/java/com/example/figmadaniel/InformasiIndividuStep2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.figmadaniel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InformasiIndividuStep2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_informasi_individu_step2)
+    }
+}
